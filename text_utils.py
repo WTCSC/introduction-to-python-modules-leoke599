4b7e2baf8471bfeb354fbd31d908e22e546d1e97
leoke599 <leoke073@gmail.com>
2024-09-12 08:59:45 -0600

    I am done with this exercise. It was really confusing at times

--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,13 @@
 def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+    chars = len(list(text)) # splits the file into a list and counts all of them
+    return chars
+    
 
 def count_words(text):
-    """
-    Count the number of words in a text
-    """
+    words = len(text.split()) # splits the text by any whitespace and counts them
+    return words
 
 def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+    import re
+    sent = [x for x in re.split("[.|!|?]", text) if x!=""] # i found this online and im not 100% sure about what everything does but i know the re.split splits the text into whatever the first argument is. the | is like an or statement and the second argument is the thing that needs to be split. the if x!="" is used to ignores empty characters
+    return len(sent)
